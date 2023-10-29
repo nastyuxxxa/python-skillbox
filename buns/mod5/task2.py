55b7dec43d2bdb480bfdcc5b5e5ed4c454c55dd0
nastyuxxxa <130449872+nastyuxxxa@users.noreply.github.com>
2023-10-30 01:36:44 +0600

    Update task2.py

--- buns/mod5/task2.py
+++ buns/mod5/task2.py
@@ -7,30 +7,49 @@ class Node:
 
 class Queue:
     def __init__(self):
-        self.first = None
-        self.last = None
+        self.start = None
+        self.end = None
+
+    def dequeue(self):
+        if self.start is None:
+            return None
+        else:
+            temp = self.start.data
+            self.start = self.start.next
+            self.start.prev = None
+            return temp
 
     def enqueue(self, val):
         new_node = Node(val)
-        if self.last is None:
-            self.first = new_node
-            self.last = new_node
+        if self.end is None:
+            self.start = new_node
+            self.end = new_node
         else:
-            self.last.next = new_node
-            new_node.prev = self.last
-            self.last = new_node
+            self.end.next = new_node
+            new_node.prev = self.end
+            self.end = new_node
 
-    def dequeue(self):
-        if self.first is None:
-            return None
-        else:
-            temp = self.first.data
-            self.first = self.first.next
-            self.first.prev = None
-            return temp
+    def insert(self, n, val):
+        if n <= 0 or self.start is None:
+            self.enqueue(val)
+            return
+        new_node = Node(val)
+        current_node = self.start
+        for _ in range(n - 1):
+            if current_node.next:
+                current_node = current_node.next
+            else:
+                break
+        new_node.prev = current_node
+        new_node.next = current_node.next
+        if current_node.next:
+            current_node.next.prev = new_node
+        current_node.next = new_node
+        if new_node.next is None:
+            self.end = new_node
 
     def print(self):
-        current = self.first
+        current = self.start
         while current:
             print(current.data, end=" ")
             current = current.next
